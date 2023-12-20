73672817493cad0f65413b15cadc0d65a859a76d
Hafsa AIT EL ATIK <hafsaaitelatik@gmail.com>
2023-12-20 18:10:23 +0000

    task 2

--- CALCULATOR.py
+++ CALCULATOR.py
@@ -34,3 +34,24 @@ window.title("Simple Calculator")
 entry_num1 = tk.Entry(window, width=10)
 entry_num1.grid(row=0, column=0, padx=10, pady=10)
 
+operator_var = tk.StringVar()
+operator_var.set("+")  # default operator
+
+operator_menu = tk.OptionMenu(window, operator_var, "+", "-", "*", "/")
+operator_menu.grid(row=0, column=1, padx=10, pady=10)
+
+entry_num2 = tk.Entry(window, width=10)
+entry_num2.grid(row=0, column=2, padx=10, pady=10)
+
+# Button to perform calculation
+calculate_button = tk.Button(window, text="Calculate", command=calculate)
+calculate_button.grid(row=1, column=0, columnspan=3, pady=10)
+
+# Label to display the result
+result_label = tk.Label(window, text="Result: ")
+result_label.grid(row=2, column=0, columnspan=3, pady=10)
+
+# Run the Tkinter event loop
+window.mainloop()
+
+
